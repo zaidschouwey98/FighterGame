ca97c41f0e1562448afd596063850be12a3d3ce9
Schouwey <zaid.schouwey@gmail.com>
2025-09-11 00:06:24 +0200

    basic gameplay finished

--- client/src/core/CameraService.ts
+++ client/src/core/CameraService.ts
@@ -4,13 +4,12 @@ import { CAMERA_ZOOM, TILE_SIZE } from "../constantes";
 export class CameraService {
     public x = 0;
     public y = 0;
-    private smoothness = 0.1; // entre 0 (lent) et 1 (instantané)
+    private smoothness = 0.05; // entre 0 (lent) et 1 (instantané)
 
     follow(position:Position, screenWidth: number, screenHeight: number) {
         const targetX = screenWidth / 2 - position.x * this.zoom - TILE_SIZE;
         const targetY = screenHeight / 2 - position.y * this.zoom - TILE_SIZE;
 
-        // Interpolation linéaire
         this.x += (targetX - this.x) * this.smoothness;
         this.y += (targetY - this.y) * this.smoothness;
     }

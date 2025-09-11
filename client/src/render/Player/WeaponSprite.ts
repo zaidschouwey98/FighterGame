8d00ac945574d3b22d08d2336dcf45ec7a580f2f
Schouwey <zaid.schouwey@gmail.com>
2025-09-11 19:49:18 +0200

    update spritesheets for sword

--- client/src/render/Player/WeaponSprite.ts
+++ client/src/render/Player/WeaponSprite.ts
@@ -10,7 +10,9 @@ export class WeaponSprite {
 
   constructor(spriteSheets: Spritesheet[], playerContainer: Container) {
     this.sprite = new Sprite(new Texture(findTexture(spriteSheets, "sword")));
-    this.sprite.anchor.set(1, 0.4);
+    this.sprite.anchor.set(0.92, 0.5);
+    this.sprite.y += 3;
+    this.sprite.zIndex = 1;
     this.sprite.rotation = 0.3;
     playerContainer.addChild(this.sprite);
   }
@@ -19,18 +21,22 @@ export class WeaponSprite {
     switch (direction) {
       case Direction.BOTTOM:
         this.sprite.scale.x = 1;
-        this.sprite.rotation = Math.PI / 2 - 0.3;
+        this.sprite.zIndex = 1;
+        this.sprite.rotation = 0.3;
         break;
       case Direction.TOP:
+        this.sprite.zIndex = -1;
         this.sprite.scale.x = 1;
-        this.sprite.rotation = -Math.PI / 2 - 0.3;
+        this.sprite.rotation = Math.PI + 0.3;
         break;
       case Direction.RIGHT:
         this.sprite.scale.x = 1;
+        this.sprite.zIndex = 1;
         this.sprite.rotation = -0.3;
         break;
       case Direction.LEFT:
         this.sprite.scale.x = -1;
+        this.sprite.zIndex = 1;
         this.sprite.rotation = 0.3;
         break;
     }

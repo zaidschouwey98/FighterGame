5e5fdfdf7e792f9aa14a2976bb5ba4a856b26258
Schouwey <zaid.schouwey@gmail.com>
2025-09-20 15:24:52 +0200

    weapon shadow

--- client/src/render/Player/weapon/WeaponSprite.ts
+++ client/src/render/Player/weapon/WeaponSprite.ts
@@ -10,23 +10,38 @@ import { createWeaponAnimations } from "./CreateWeaponAnimation";
 
 export class WeaponSprite {
   private sprite: Sprite;
+  private shadowSprite: Sprite;
   private controller: WeaponAnimController;
   private currentDirection: Direction = Direction.BOTTOM;
 
-  constructor(spriteSheets: Spritesheet[],_staticEffectContainer:Container, playerContainer: Container, type: WeaponType, skin = "default", anchor: {x:number,y:number}, posX:number, posY:number) {
+  constructor(spriteSheets: Spritesheet[], _staticEffectContainer: Container, playerContainer: Container, type: WeaponType, skin = "default", anchor: { x: number, y: number }, posX: number, posY: number) {
     console.log(skin)
     this.sprite = new Sprite(findTexture(spriteSheets, type as any));
     this.sprite.anchor = anchor;
     this.sprite.x = posX;
     this.sprite.y = posY;
     this.sprite.label = "weapon";
+
+    const groundShadow = new Sprite(findTexture(spriteSheets, type as any));
+    groundShadow.label = "weaponShadow";
+    groundShadow.anchor = anchor;
+    groundShadow.x = this.sprite.x;
+    groundShadow.y = this.sprite.y;
+    groundShadow.tint = 0x000000;
+    groundShadow.alpha = 0.3;
+    groundShadow.scale.y = 1; // aplatie
+    groundShadow.zIndex = -2;
+    this.shadowSprite = groundShadow;
+ 
+    playerContainer.addChild(this.shadowSprite);
     playerContainer.addChild(this.sprite);
 
-    this.controller = new WeaponAnimController(createWeaponAnimations(this.sprite, type,playerContainer,spriteSheets));
+
+    this.controller = new WeaponAnimController(createWeaponAnimations(this.sprite, type, playerContainer, spriteSheets));
   }
 
-  setState(state: PlayerState, comboIndex = 0, mouseDirection:{x:number,y:number}) {
-    this.controller.setState(state, comboIndex,mouseDirection);
+  setState(state: PlayerState, comboIndex = 0, mouseDirection: { x: number, y: number }) {
+    this.controller.setState(state, comboIndex, mouseDirection);
   }
 
   setDirection(direction: Direction) {
@@ -37,6 +52,13 @@ export class WeaponSprite {
   }
 
   update(delta: number) {
+    
+    //if scale.x > 0 && scale.y
+    this.shadowSprite.scale = this.sprite.scale;
+    this.shadowSprite.rotation = this.sprite.rotation;
+    this.shadowSprite.x = this.sprite.x;
+    this.shadowSprite.y = this.sprite.y + 4;
+    this.shadowSprite.visible = this.sprite.visible;
     this.controller.update(delta);
   }
 }
